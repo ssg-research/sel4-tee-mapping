51b8fbdd35663fcc26877855d41e9c8709cbe076
Max Crone <max.crone@aalto.fi>
2021-03-15 16:24:52 +0200

    Add script for compiling with TEE support

---
+++ compile.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Missing argument: please provide build directory"
+	exit -1
+fi
+
+echo ":: Compiling TEE in $1"
+
+if [ ! -f $1/resize_dataports.py ]; then
+	ln -s $(pwd)/resize_dataports.py $1/resize_dataports.py
+fi
+
+if [ ! -f $1/remap_dataports.py ]; then
+	ln -s $(pwd)/remap_dataports.py $1/remap_dataports.py
+fi
+
+echo ":: Installed symbolic links in build directory"
+
+echo ":: Running first build"
+
+cd $1
+
+ninja
+
+echo ":: Resizing dataports"
+
+python3 resize_dataports.py --spec $(pwd)/tee.cdl --header $(pwd)/../projects/camkes/apps/tee/include/buffer.h 
+
+echo ":: Running build (to generate new spec with correct number of dataport frames)"
+
+ninja
+
+echo ":: Mapping TA code pages into the Attestation component's VSpace"
+
+python3 remap_dataports.py  --spec $(pwd)/capdl_spec.c
+
+echo ":: Running final build"
+
+ninja
+
+echo ":: Done"
