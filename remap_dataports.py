1ad842fd5f532d8ab4278044303078480c051f14
Max Crone <max.crone@aalto.fi>
2021-03-25 11:40:10 +0200

    Fix matching inaccuracy of TA code frames
    The matching was ambiguous, causing the wrong filedata frames to be
    mapped into a dataport for a specific TA. To replicate: assume we have
    two TA's named "ta" and "ta2". Before this fix, finding all code pages
    belonging to "ta" involved a search for `.name = "frame_ta`, but this
    would match entries `.name = "frame_ta2` too. This is easily fixed by
    including the `_group_bin` suffix in our search.

--- remap_dataports.py
+++ remap_dataports.py
@@ -23,34 +23,34 @@ with open(file_path, 'r') as f:
 
 for ta_name in ta_names:
     connection_name = '{}_binary'.format(ta_name)
-
+    
     # 1. Find all code frames of the TA
-
+    
     # a. First retrieve all frames of the TA, later prune out irrelevant frames
     #    Note that we can not just findall, because these frames will be mentioned
     #    at other spots too.
-    nr_ta_frames = spec.count('.name = "frame_{}'.format(ta_name))
+    nr_ta_frames = spec.count('.name = "frame_{}_group_bin'.format(ta_name))
     offset = 0
     frame_indices = []
     for _ in range(nr_ta_frames):
-        i = spec.find('.name = "frame_{}'.format(ta_name), offset)
+        i = spec.find('.name = "frame_{}_group_bin'.format(ta_name), offset)
         frame_indices.append(i)
         offset = i + 1
-
+    
     # b. Loop over frame_indices and save indices of the ones with FileData
     filedata_frame_indices = []
     for i in frame_indices:
         post = spec[i:i+200]
         if post.find('CDL_FrameFill_FileData') != -1:
             filedata_frame_indices.append(i)
-
+    
     # c. Get object IDs of the code frames
     code_obj_ids = []
     for i in filedata_frame_indices:
         pre = spec[i-40:i]
         obj_id = pre[pre.find('[')+1:pre.find(']')]
         code_obj_ids.append(obj_id)
-
+    
     # 2. Find dataport Frame Objects
     #dataport_frames = [i for i in findall('.name = "{}_data'.format(connection_name), spec)]
     count_dataport_frames = spec.count('.name = "{}_data'.format(connection_name))
@@ -60,19 +60,19 @@ for ta_name in ta_names:
         pre = spec[offset-40:offset]
         obj_id = pre[pre.find('[')+1:pre.find(']')]
         dataport_obj_ids.append(obj_id)
-
+    
     assert len(code_obj_ids) >= len(dataport_obj_ids)
-
+    
     # 3. Find cap for dataport in the Attestation component
-
+    
     # a. First find index of start of Attestation capability group
     index_attest_caps = spec.find('.name = "pt_attestation_group_bin')
-
+    
     # b. Find Attestation's FrameCaps for dataports
     framecaps = [index_attest_caps + i for i in findall('/* {}_data'.format(connection_name), spec[index_attest_caps:])]
     # Select only those that are part of Attestation capabilities
     framecaps = framecaps[0:count_dataport_frames]
-
+    
     # 4. Insert obj_id for code frame into dataport caps
     for i in range(len(dataport_obj_ids)):
         area = spec[framecaps[i]-16:framecaps[i]]
